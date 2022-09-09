df4c58a7f62f8b1b72e2a22cccb37292ac150fb0
Maxime Debruyne <mdebruynepro@gmail.com>
2022-09-09 11:40:18 +0200

    Patien sans css

---
+++ patien.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Patien</title>
+</head>
+<body>
+    <h1>Patient</h1>
+    <form action="" method="post">
+
+        <label for="civ">Civ :</label>
+        <input type="text" id="civ">
+
+        <label for="nom">Nom de naissance :</label>
+        <input type="text" id="name">
+
+        <label for="nomépouse">Nom dépouse :</label>
+        <input type="text" id="name">
+
+        <label for="prenom">Prénom :</label>
+        <input type="text" id="prenom">
+
+        <label for="dnaissance">Date de naissance:</label>
+        <input type="text" id="birsday">
+
+        <label for="addresse">Addresse:</label>
+        <input type="text" id="addresse">
+
+        <label for="cp">CP :</label>
+        <input type="text" id="cp">
+
+        <label for="ville">Ville :</label>
+        <input type="text" id="ville">
+
+        <label for="email">Email :</label>
+        <input type="text" id="email">
+
+        <label for="telephone">Téléphone:</label>
+        <input type="text" id="telephone">
+
+        <input type="submit" id="submit">
+    </form>
+</body>
+</html>
