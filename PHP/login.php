1332f5636a9320920550e95549e92d3d3a7eebe5
Charoxy <charoxy59@gmail.com>
2022-12-09 09:05:22 +0100

    Mise a jour

--- PHP/login.php
+++ PHP/login.php
@@ -3,15 +3,32 @@
     $mdp = $_POST["mdp"];
     echo "$id";
     echo "$mdp";
+    session_start();
+
+
     try{
         $conn = new PDO('mysql:host=localhost;dbname=Hopitale', 'Dev' , 'Sio2021*');
         $stmt = $conn->prepare('SELECT * FROM personnel WHERE id=:id and mdp=:mdp;');
         $stmt->execute([":id"=>$id,":mdp"=>$mdp]);
                 
     foreach ($stmt as $row) {
-        echo "1";
-        header("Location: dashboard.php");
-        die();
+
+        $_SESSION['services'] = $row['num_serv'];
+
+        switch($row['num_serv']){
+            case 01:
+                //   Medecin
+                die();
+            case 02:
+                //  ADMIN
+                header("Location: dashboard.php");
+                die();
+            case 03:
+                // Sectretaire
+                header("Location: pre-admission.php");
+                die();
+            }
+        
         }
         header("Location: ../index.php");
     }
