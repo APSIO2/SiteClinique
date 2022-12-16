2b3f40585d4c026b49cc46d631d0629fd9caf9a5
alexers4 <112950088+alexers4@users.noreply.github.com>
2022-12-16 11:33:46 +0100

    update_pread

--- PHP/updatePread.php
+++ PHP/updatePread.php
@@ -9,11 +9,27 @@ if(!isset($_SESSION['services'])){
 
 
 $num_op = $_GET['num_op'];
-echo "$num_op";
+$nom_med = $_GET['nom_med'];
+$date_op = $_GET['date_op'];
+$heure_op = $_GET['heure_op'];
+$pread = $_GET['pre_ad'];
+$nom_nai = $_GET['nom_nai'];
+
 
 try{
     $conn = new PDO('mysql:host=localhost;dbname=Hopitale', 'Dev' , 'Sio2021*');
-    $stmt2 = $conn->query("DELETE FROM `operation` WHERE operation.num_op = $num_op");
+    $stmt = $conn->query("SELECT * FROM `personnel`");
+    $stmt2 = $conn->query("SELECT * FROM `patient`");
+
+    foreach($stmt as $row){
+        $num_med = $row["num_med"];
+    }
+
+    foreach($stmt2 as $row){
+        $num_secu = $row["num_secu"];
+    }
+
+    $stmt3 = $conn->query("UPDATE `operation` SET `num_op`=$num_op,`num_med`=$num_med,`num_secu`=$num_secu,`date_op`=$date_op,`heure_op`=$heure_op,`pre_admission`=$pread WHERE operation.num_op = $num_op");
 }
 catch(PDOException $e){}
 
