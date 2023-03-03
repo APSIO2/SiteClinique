01b902ad9e373a38ecdac4d62776723af4b4e6a4
alexers4 <112950088+alexers4@users.noreply.github.com>
2023-03-03 08:36:05 +0100

    Update gestionPread.php

--- PHP/gestionPread.php
+++ PHP/gestionPread.php
@@ -40,7 +40,23 @@
                 
                 echo "<form action='gestionPread.php' class=''>";
                 echo '<p class="textpread">'. $num_op .'</p><br>';
-                echo '<p class="textpread">Médecin :</p><input type="text" value="'. $nom_med .'"> <br>';
+                echo '<select name="nom_med" id="" class="fullform">';
+                    try{
+                        $conn = new PDO('mysql:host=localhost;dbname=Hopitale', 'Dev' , 'Sio2021*');
+                        $stmt = $conn->prepare('SELECT * FROM personnel WHERE num_serv="01";');
+                        $stmt->execute();
+                                
+                    foreach ($stmt as $row) {
+                        $prenom = $row[2];
+                        echo "<option>" . $prenom . "</option>";
+                        }
+                        echo "1";
+                    }
+                    catch(PDOException $e){
+                        echo $e->getMessage();
+                    }
+
+                echo '</select>';
                 echo '<input type="date" value="'. $date_op .'"> <br>';
                 echo '<input type="time" value="'. $heure_op .'"> <br>';
                 echo '<input type="text" value="'. $pre_ad .'"> <br>';
