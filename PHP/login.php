94d0ad218eb3fd03f2876c41f1265b201190e759
alexers4 <112950088+alexers4@users.noreply.github.com>
2023-03-17 11:55:58 +0100

    captcha

--- PHP/login.php
+++ PHP/login.php
@@ -39,4 +39,14 @@
     catch(PDOException $e){
         echo $e->getMessage();
     }
+
+    if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"])
+  {
+    $status = "<p style='color:#FFFFFF; font-size:20px'>
+    <span style='background-color:#46ab4a;'>Votre code captcha est correct.</span></p>"; 
+  }else{
+    $status = "<p style='color:#FFFFFF; font-size:20px'>
+    <span style='background-color:#FF0000;'>Le code captcha entré ne correspond pas! Veuillez réessayer.</span></p>";
+  }
+  echo $status;
 ?>
