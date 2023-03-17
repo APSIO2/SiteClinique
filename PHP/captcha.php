107d6834a1685a0fbfd98f815508c2812b3482ab
alexers4 <112950088+alexers4@users.noreply.github.com>
2023-03-17 13:44:26 +0100

    captcha css

--- PHP/captcha.php
+++ PHP/captcha.php
@@ -3,7 +3,7 @@
     $code=rand(1000,9999);
     $_SESSION["code"]=$code;
     $image = imagecreatetruecolor(50, 24);
-    $background = imagecolorallocate($image, 245, 73, 73); 
+    $background = imagecolorallocate($image, 168, 175, 143); 
     $forground = imagecolorallocate($image, 255, 255, 255);
     imagefill($image, 0, 0, $background);
     imagestring($image, 5, 5, 5,  $code, $forground);
--- PHP/login.php
+++ PHP/login.php
@@ -2,8 +2,6 @@
     require("ConnexionBdd.php");
     $id = $_POST["id"];
     $mdp = $_POST["mdp"];
-    echo "$id";
-    echo "$mdp";
     session_start();
 
 
@@ -12,41 +10,40 @@
         $stmt = $conn->prepare('SELECT * FROM personnel WHERE id=:id and mdp=:mdp;');
         $stmt->execute([":id"=>$id,":mdp"=>$mdp]);
 
+        if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"]){
+            $status = "<p style='color:#FFFFFF; font-size:20px'>
+            <span style='background-color:#46ab4a;'>Votre code captcha est correct.</span></p>"; 
+            foreach ($stmt as $row) {
+
+                $_SESSION['role'] = $row['num_role'];
+    
+                print($row['num_role']);
+    
+                switch($row['num_role']){
+                    case 01:
+                        header("Location: medecin.php");
+                        $_SESSION['id'] = $row['num_med'];
+                        die();
+                    case 02:
+                        //  ADMIN
+                        header("Location: dashboard.php");
+                        die();
+                    case 03:
+                        // Sectretaire
+                        header("Location: dashboardSecretaire.php");
+                        die();
+                    }
                 
-        foreach ($stmt as $row) {
-
-            $_SESSION['role'] = $row['num_role'];
-
-            print($row['num_role']);
-
-            switch($row['num_role']){
-                case 01:
-                    header("Location: medecin.php");
-                    $_SESSION['id'] = $row['num_med'];
-                    die();
-                case 02:
-                    //  ADMIN
-                    header("Location: dashboard.php");
-                    die();
-                case 03:
-                    // Sectretaire
-                    header("Location: dashboardSecretaire.php");
-                    die();
                 }
-            
-            }
+        }
+        else{
+            $status = "<p style='color:#FFFFFF; font-size:20px'>
+            <span style='background-color:#FF0000;'>Le code captcha entré ne correspond pas! Veuillez réessayer.</span></p>";
+        }
+        echo $status;
+
     }
     catch(PDOException $e){
         echo $e->getMessage();
     }
-
-    if(isset($_POST["captcha"])&&$_POST["captcha"]!=""&&$_SESSION["code"]==$_POST["captcha"]){
-        $status = "<p style='color:#FFFFFF; font-size:20px'>
-        <span style='background-color:#46ab4a;'>Votre code captcha est correct.</span></p>"; 
-    }
-    else{
-        $status = "<p style='color:#FFFFFF; font-size:20px'>
-        <span style='background-color:#FF0000;'>Le code captcha entré ne correspond pas! Veuillez réessayer.</span></p>";
-    }
-    echo $status;
 ?>
